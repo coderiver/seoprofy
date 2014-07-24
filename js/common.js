1ec0d2af184b53628bcc9884ba4cea74c1c84024
hraboviyvadim <hraboviyvadim@gmail.com>
2014-07-24 14:45:58 +0300

    new blocks

--- js/common.js
+++ js/common.js
@@ -5,9 +5,32 @@ head.ready(function() {
 	
 	$('.js-portfolio-slider').slick({
 		dots: true,
-		infinite: true,
-		slidesToShow: 3,
-		slidesToScroll: 3
+		centerMode: true,
+		centerPadding: 40,
+		slidesToShow: 3
 	});
+
+// tabs
+	function tab() {
+       $(".js-tab").each(function(){
+         var tab_link = $(this).find("a");
+         var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+         tab_cont.hide();
+         $(this).parents(".js-tab-group").find(".js-tab1").show();
+            tab_link.on("click", function() {
+             	var index = $(this).attr("href");
+             	tab_link.removeClass("is-active");
+             	tab_link.parent().removeClass("is-active");
+             	$(this).addClass("is-active");
+             	$(this).parent().addClass("is-active");
+             	tab_cont.hide();
+             	$(this).parents(".js-tab-group").find("."+index).show();
+                return false;
+            });
+        });
+    }
+    if ($(".js-tab-group").length) {
+    	tab();
+    };	
 
 });
